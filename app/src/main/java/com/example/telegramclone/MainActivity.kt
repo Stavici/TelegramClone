8175c40172b61ffef7592f28073925ac64118968
Stanislav <stanis1av.b@yandex.ru>
2023-11-15 12:35:29 +0300

    Add Drawer, Header, main Tool Bar

--- app/src/main/java/com/example/telegramclone/MainActivity.kt
+++ app/src/main/java/com/example/telegramclone/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.telegramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.activity.SystemBarStyle
+import com.example.telegramclone.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainDrawer: Drawer
+    private lateinit var mainHeader: AccountHeader
+    private lateinit var mainToolBar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFileds()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mainToolBar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mainDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mainToolBar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mainHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mainHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Stanislav Kovnatskiy")
+                    .withEmail("+79993334455")
+            ).build()
+    }
+
+    private fun initFileds() {
+        mainToolBar = mainBinding.mainToolBar
+
     }
 }
